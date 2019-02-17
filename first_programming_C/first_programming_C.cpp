c922e9d3989c987ca18dcab9e96c5dc80b43292d
rechardchen123 <rechardchen123@outlook.com>
2019-02-17 23:36:29 +0000

    new functions

---
+++ first_programming_C/first_programming_C.cpp
@@ -0,0 +1,24 @@
+// first_programming_C.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string user_name;
+	string first_name;
+	cout << "Please enter your first name: ";
+	cin >> user_name;
+	cout << "please enter your family name: ";
+	cin >> first_name;
+	cout << "\n";
+	cout << "hello, ";
+	cout << user_name<< first_name<<endl;
+	cout << "... and goodbye!\n";
+
+	return 0;
+}
+
+
+
