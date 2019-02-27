1a0f0440b071cb36da460815acb9424791b5216b
rechardchen123 <rechardchen123@outlook.com>
2019-02-27 23:43:03 +0000

    reverse list

--- first_programming_C/first_programming_C.cpp
+++ first_programming_C/first_programming_C.cpp
@@ -1,6 +1,3 @@
-// first_programming_C.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -19,6 +16,3 @@ int main()
 
 	return 0;
 }
-
-
-
